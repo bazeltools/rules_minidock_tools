44f6d16e43fc4dfec7eb8c5855c0085a56c729d6
Ian O Connell <ianoc@ianoc.net>
2022-05-17 16:59:52 -0600

    Add some initial scripts

---
+++ .github/workflows/build_binaries.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+
+TARGET_OS=$1
+shift
+if [ -z "$TARGET_OS" ]; then
+    echo "Need a target OS"
+    exit 1
+fi
+
+
+TARGET_PLATFORM=$1
+shift
+if [ -z "$TARGET_PLATFORM" ]; then
+    echo "Need a TARGET_PLATFORM arg, e.g. x86, or aarch64"
+    exit 1
+fi
+
+if [[ "$TARGET_OS" =~ macos.* ]]; then
+    export TARGET_OS=macos
+    export SDKROOT=$(xcrun -sdk macosx --show-sdk-path)
+    export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx --show-sdk-platform-version)
+fi
+
+CROSS_BUILD_TARGET=""
+APP_DIR="target/release/"
+if [ "$TARGET_PLATFORM" != "x86_64" ];then
+    if [ "$TARGET_OS" == "macos" ]; then
+        CROSS_BUILD_TARGET="--target=aarch64-apple-darwin"
+        APP_DIR="target/aarch64-apple-darwin/release/"
+    else
+        echo "Don't know how to build $TARGET_PLATFORM on $TARGET_OS"
+        exit 1
+    fi
+fi
+
+
+for b in "$@"; do
+    set -x
+    cargo build $CROSS_BUILD_TARGET --bin $b --release --all-features
+
+    OUTPUT_ASSET_NAME="${b}-$TARGET_OS-${TARGET_PLATFORM}"
+    cp $APP_DIR/$b $OUTPUT_ASSET_NAME
+    GENERATED_SHA_256=$(shasum -a 256 $OUTPUT_ASSET_NAME | awk '{print $1}')
+    echo $GENERATED_SHA_256 > ${OUTPUT_ASSET_NAME}.sha256
+    tag_name="${GITHUB_REF##*/}"
+    hub release edit -a $OUTPUT_ASSET_NAME -a $GENERATED_SHA_256  -m "" "$tag_name"
+done
+
